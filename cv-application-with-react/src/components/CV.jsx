404a764d4f406868ca24b9d3f3e74548a111c5bf
catalinbadila <112496548+cbd23@users.noreply.github.com>
2024-04-27 13:38:36 +0300

    Import & add General, Projects & Education.

--- cv-application-with-react/src/components/CV.jsx
+++ cv-application-with-react/src/components/CV.jsx
@@ -1,9 +1,14 @@
 import './../styles/CV.css'
+import { General } from './General'
+import { ProjectsSection } from './ProjectsSection'
+import { EducationSection } from './EducationSection'
 
 export function CV() {
     return (
         <div className='cv'>
-
+            <General></General>
+            <ProjectsSection></ProjectsSection>
+            <EducationSection></EducationSection>
         </div>
     )
 }
