79acff65c21e27d5c239f266d6724c0730ee8f80
catalinbadila <112496548+cbd23@users.noreply.github.com>
2024-04-30 17:23:34 +0300

    Add a state variable that stores general info.

--- cv-application-with-react/src/App.jsx
+++ cv-application-with-react/src/App.jsx
@@ -3,13 +3,23 @@ import './App.css'
 import { EditingPanel } from './components/EditingPanel'
 import { CV } from './components/CV'
 
-
 function App() {
+  const [generalInfo, setGeneralInfo] = useState({
+    firstName: 'Frank',
+    lastName: 'Martin',
+    position: 'Frontend Dev',
+    city: 'California',
+  })
+
+  // Create a fn that will be called by onChange event handlers to update 'generalInfo'
+  const updateGeneralInfo = (updatedInfo) => {
+    setGeneralInfo(updatedInfo)
+  }
 
   return (
     <div className='app-container'>
-      <EditingPanel></EditingPanel>
-      <CV></CV>
+      <EditingPanel updateGeneralInfo={updateGeneralInfo}></EditingPanel>
+      <CV generalInfo={generalInfo}></CV>
     </div>
   )
 
