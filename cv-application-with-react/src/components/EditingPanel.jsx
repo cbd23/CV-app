1661f1eb9a0b23851aedbb141508e9b5d1d62a1b
catalinbadila <112496548+cbd23@users.noreply.github.com>
2024-04-30 17:27:44 +0300

    Pass the updateGeneralInfo to EditGeneral.

--- cv-application-with-react/src/components/EditingPanel.jsx
+++ cv-application-with-react/src/components/EditingPanel.jsx
@@ -1,9 +1,10 @@
 import './../styles/EditingPanel.css'
+import { EditGeneral } from './EditGeneral'
 
-export function EditingPanel() {
-    return(
+export function EditingPanel({ updateGeneralInfo }) {
+    return (
         <div className='editing-panel'>
-
+            <EditGeneral updateGeneralInfo={updateGeneralInfo}></EditGeneral>
         </div>
     )
 }
