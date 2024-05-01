71167690606265b2cc742755c6326c91ef2b1ab3
catalinbadila <112496548+cbd23@users.noreply.github.com>
2024-05-01 20:09:52 +0300

    Add generalInfo as a prop & add onChange for every input.

--- cv-application-with-react/src/components/EditGeneral.jsx
+++ cv-application-with-react/src/components/EditGeneral.jsx
@@ -1,49 +1,50 @@
 import './../styles/EditGeneral.css'
 
-export function EditGeneral({ updateGeneralInfo }) {
+export function EditGeneral({ updateGeneralInfo, generalInfo }) {
     const handleInputChange = (e) => {
-        const updatedInfo = { ...updateGeneralInfo, [e.target.id]: e.target.value}
+        const updatedInfo = { ...generalInfo, [e.target.id]: e.target.value}
         updateGeneralInfo(updatedInfo)
     }
     return (
         <div className='edit-general-container'>
+            <h2>General Info</h2>
             <div className="full-name-container general-div">
                 <div className="first-name-container">
-                    <label htmlFor="firstName">First name*</label>
-                    <input onChange={(e) => handleInputChange(e)} type="text" id='firstName' placeholder='Frank' required />
+                    <label htmlFor="firstName">First name</label>
+                    <input onChange={(e) => handleInputChange(e)} type="text" id='firstName' maxLength='15' placeholder='Cătălin' />
                 </div>
                 <div className="last-name-container">
-                    <label htmlFor="lastName">Last name*</label>
-                    <input type="text" id='lastName' placeholder='Martin' required />
+                    <label htmlFor="lastName">Last name</label>
+                    <input onChange={(e) => handleInputChange(e)} type="text" id='lastName' maxLength='15' placeholder='Bădilă' />
                 </div>
             </div>
             <div className="position-container general-div">
-                <label htmlFor="position">Position*</label>
-                <input type="text" id='position' placeholder='Frontend Developer' required />
+                <label htmlFor="position">Position</label>
+                <input onChange={(e) => handleInputChange(e)} type="text" id='position' maxLength='40' placeholder='Frontend Developer' />
             </div>
             <div className="city-container general-div">
-                <label htmlFor="city">City*</label>
-                <input type="text" id='city' placeholder='California' required />
+                <label htmlFor="city">City</label>
+                <input onChange={(e) => handleInputChange(e)} type="text" id='city' maxLength='20' placeholder='Bucharest' />
             </div>
             <div className="phone-container general-div">
-                <label htmlFor="phone">Phone*</label>
-                <input type="tel" id='phone' placeholder='0123 456 789' required />
+                <label htmlFor="phone">Phone</label>
+                <input onChange={(e) => handleInputChange(e)} type="tel" id='phone' maxLength='15' placeholder='0123 456 789' />
             </div>
             <div className="email-container general-div">
-                <label htmlFor="email">Email*</label>
-                <input type="email" id='email' placeholder='frank.martin@example.com' required />
+                <label htmlFor="email">Email</label>
+                <input onChange={(e) => handleInputChange(e)} type="email" id='email' maxLength='30' placeholder='catalin@example.com' />
             </div>
             <div className="github-link-container general-div">
                 <label htmlFor="github">Github</label>
-                <input type="text" id='github' placeholder='https://github.com/frankmartin23' />
+                <input onChange={(e) => handleInputChange(e)} type="text" id='github' placeholder='https://github.com/cbd23' />
             </div>
             <div className="linkedin-link-container general-div">
                 <label htmlFor="linkedin">Linkedin</label>
-                <input type="text" id='linkedin' placeholder='https://www.linkedin.com/in/frank-martin/' />
+                <input onChange={(e) => handleInputChange(e)} type="text" id='linkedin' placeholder='https://www.linkedin.com/in/catalin-badila/' />
             </div>
             <div className="bio-container general-div">
-                <label htmlFor="bio">About*</label>
-                <textarea rows='5' maxLength='320' id='bio' placeholder='Write an intro about yourself' />
+                <label htmlFor="bio">About</label>
+                <textarea onChange={(e) => handleInputChange(e)} rows='5' maxLength='320' id='bio' placeholder='Write an intro about yourself' />
             </div>
         </div>
     )
