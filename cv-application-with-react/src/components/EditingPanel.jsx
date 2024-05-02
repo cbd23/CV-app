09352d89cd7ae085658569436068642bceccd86b
catalinbadila <112496548+cbd23@users.noreply.github.com>
2024-05-02 19:16:43 +0300

    Pass projects' state & setter fn to EditProjects.

--- cv-application-with-react/src/components/EditingPanel.jsx
+++ cv-application-with-react/src/components/EditingPanel.jsx
@@ -2,11 +2,11 @@ import './../styles/EditingPanel.css'
 import { EditGeneral } from './EditGeneral'
 import { EditProjects } from './EditProjects'
 
-export function EditingPanel({ updateGeneralInfo, generalInfo }) {
+export function EditingPanel({ updateGeneralInfo, generalInfo, updateProjectsInfo, projectsInfo }) {
     return (
         <div className='editing-panel'>
             <EditGeneral updateGeneralInfo={updateGeneralInfo} generalInfo={generalInfo}></EditGeneral>
-            <EditProjects></EditProjects>
+            <EditProjects updateProjectsInfo={updateProjectsInfo} projectsInfo={projectsInfo}></EditProjects>
         </div>
     )
 }
