e93822265ae32a5edba92c6ffa2d3cee5d206e16
catalinbadila <112496548+cbd23@users.noreply.github.com>
2024-05-04 17:30:43 +0300

    Import & add EditEducation.

--- cv-application-with-react/src/components/EditingPanel.jsx
+++ cv-application-with-react/src/components/EditingPanel.jsx
@@ -1,12 +1,14 @@
 import './../styles/EditingPanel.css'
 import { EditGeneral } from './EditGeneral'
 import { EditProjects } from './EditProjects'
+import { EditEducation } from './EditEducation'
 
-export function EditingPanel({ updateGeneralInfo, generalInfo, updateProjectsInfo, projectsInfo }) {
+export function EditingPanel({ updateGeneralInfo, generalInfo, updateProjectsInfo, projectsInfo, updateEducationInfo, educationInfo }) {
     return (
         <div className='editing-panel'>
             <EditGeneral updateGeneralInfo={updateGeneralInfo} generalInfo={generalInfo}></EditGeneral>
             <EditProjects updateProjectsInfo={updateProjectsInfo} projectsInfo={projectsInfo}></EditProjects>
+            <EditEducation updateEducationInfo={updateEducationInfo} educationInfo={educationInfo}></EditEducation>
         </div>
     )
 }
