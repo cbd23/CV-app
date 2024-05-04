7cc4fb8ecc10dd481d0672e9c8bce34ca0c4b400
catalinbadila <112496548+cbd23@users.noreply.github.com>
2024-05-04 17:35:12 +0300

    Pass educationInfo & remove placeholders.

--- cv-application-with-react/src/components/EducationSection.jsx
+++ cv-application-with-react/src/components/EducationSection.jsx
@@ -1,25 +1,17 @@
 import './../styles/EducationSection.css'
 import { Education } from './Education'
 
-export function EducationSection() {
+export function EducationSection({ educationInfo }) {
     return (
         <div className='education-container'>
             <h4 className="education-title">EDUCATION</h4>
-            <Education 
-                school='Informal School of IT' 
-                studyField='Web Development with JavaScript'
-                startDate='FEB 2024'
-                endDate='APR 2024'
-                place='ONLINE'
-                description='Discovered new galaxies in our Universe during this 4-months course.'>
-            </Education>
-            <Education 
-                school='Informal School of IT' 
-                studyField='Web Development with JavaScript'
-                startDate='FEB 2024'
-                endDate='APR 2024'
-                place='ONLINE'
-                description='Discovered new galaxies in our Universe during this 4-months course.'>
+            <Education educationInfo={educationInfo}
+                schoolName='educationOneName' 
+                studyField={educationInfo.educationOneStudyField}
+                startDate={educationInfo.educationOneStartDate}
+                endDate={educationInfo.educationOneEndDate}
+                place={educationInfo.educationOnePlace}
+                description={educationInfo.educationOneDescription}>
             </Education>
         </div>
     )
