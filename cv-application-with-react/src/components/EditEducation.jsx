abaa8150bdce49b30ccb5d86839eff0a91f29665
catalinbadila <112496548+cbd23@users.noreply.github.com>
2024-05-04 17:38:37 +0300

    Create EditEducation & write a fn that handles Date selection.

---
+++ cv-application-with-react/src/components/EditEducation.jsx
@@ -0,0 +1,82 @@
+import './../styles/EditEducation.css'
+
+export function EditEducation({ updateEducationInfo, educationInfo }) {
+    const handleInputChange = (e) => {
+        const updatedInfo = { ...educationInfo, [e.target.id]: e.target.value}
+        updateEducationInfo(updatedInfo)
+    }
+
+    const handleDateChange = (e) => {
+        // The empty string on index 0 matches each month's numeric equivalent with their index 
+        const months = [
+            { id: '01', name: 'JAN' },
+            { id: '02', name: 'FEB' },
+            { id: '03', name: 'MAR' },
+            { id: '04', name: 'APR' },
+            { id: '05', name: 'MAY' },
+            { id: '06', name: 'JUN' },
+            { id: '07', name: 'JUL' },
+            { id: '08', name: 'AUG' },
+            { id: '09', name: 'SEPT'},
+            { id: '10', name: 'OCT' },
+            { id: '11', name: 'NOV' },
+            { id: '12', name: 'DEC' },
+        ]
+
+        // This fn returns a month's name based on month's ID
+        function getMonthName(monthId) {
+            const month = months.find(value => value.id == monthId)
+            return month.name
+        }
+
+        // We store the selected MONTH & YEAR based on the input[type='month] value
+        const selectedMonth = e.target.value.slice(5)
+        const selectedYear = e.target.value.slice(0, 4)
+
+        // Display MONTH & YEAR in a string using the desired format
+        const stringifiedDate = getMonthName(selectedMonth) + ' ' + selectedYear
+
+        // Update the state variable with the selected date
+        const updatedDate = { ...educationInfo, [e.target.id]: stringifiedDate }
+        updateEducationInfo(updatedDate)
+    }
+
+    return (
+        <div className='edit-education-container'>
+            <h2>Education Info</h2>
+            <div className="education-one-container">
+                <h3>First education</h3>
+                <div className="education-one-name education-div">
+                    <label htmlFor="educationOneName">School</label>
+                    <input onChange={(e) => handleInputChange(e)} type="text" id='educationOneName' maxLength='30' placeholder='Informal School of IT' />
+                </div>
+                <div className="education-one-study-field education-div">
+                    <label htmlFor="educationOneStudyField">Study field</label>
+                    <input onChange={(e) => handleInputChange(e)} type="text" id='educationOneStudyField' placeholder='Web Development with JavaScript' />
+                </div>
+                <div className="education-one-period education-div">
+                    <div className="start-date">
+                        <label htmlFor="educationOneStartDate">Start date</label>
+                        <input onChange={(e) => handleDateChange(e)} type='month' id='educationOneStartDate' defaultValue='2023-10' />
+                    </div>
+                    <div className="still-attending">
+                        <input type="checkbox" id='stillAttending' />
+                        <label htmlFor="stillAttending">Still attending this school</label>
+                    </div>
+                    <div className="end-date">
+                        <label htmlFor="educationOneEndDate">End date</label>
+                        <input onChange={(e) => handleDateChange(e)} type='month' id='educationOneEndDate' defaultValue='2024-02' />
+                    </div>
+                </div>
+                <div className="education-one-place education-div">
+                    <label htmlFor="educationOnePlace">Location</label>
+                    <input onChange={(e) => handleInputChange(e)} type="text" id='educationOnePlace' placeholder='BUCHAREST' />
+                </div>
+                <div className="education-one-description education-div">
+                    <label htmlFor="educationOneDescription">Description</label>
+                    <textarea onChange={(e) => handleInputChange(e)} id='educationOneDescription' maxLength='200' placeholder='Tell something about what you learned. Did you build a final project? ' />
+                </div>
+            </div>
+        </div>
+    )
+}
