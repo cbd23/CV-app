1920731f4e59b8e4de7b34f184efff5c6d84d967
catalinbadila <112496548+cbd23@users.noreply.github.com>
2024-05-04 17:29:01 +0300

    Pass educationInfo to EducationSection.

--- cv-application-with-react/src/components/CV.jsx
+++ cv-application-with-react/src/components/CV.jsx
@@ -3,12 +3,12 @@ import { General } from './General'
 import { ProjectsSection } from './ProjectsSection'
 import { EducationSection } from './EducationSection'
 
-export function CV({ generalInfo, projectsInfo }) {
+export function CV({ generalInfo, projectsInfo, educationInfo }) {
     return (
         <div className='cv'>
             <General generalInfo={generalInfo}></General>
             <ProjectsSection projectsInfo={projectsInfo}></ProjectsSection>
-            <EducationSection></EducationSection>
+            <EducationSection educationInfo={educationInfo}></EducationSection>
         </div>
     )
 }
