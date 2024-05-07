18647b6159d435b8b2025b308f58ef88a767851d
catalinbadila <112496548+cbd23@users.noreply.github.com>
2024-05-07 09:58:21 +0300

    Render Education if schoolName is not an empty string.

--- cv-application-with-react/src/components/Education.jsx
+++ cv-application-with-react/src/components/Education.jsx
@@ -3,22 +3,26 @@ import './../styles/Education.css'
 
 export function Education({ educationInfo, schoolName, studyField, startDate, endDate, place, description }) {
     return (
-        <div className="education">
+      <div className="education">
+        {educationInfo[schoolName] !== "" && (
+          <>
             <div className="education-row-one">
-                <span className="school-name">{educationInfo[schoolName]}</span>
-                &nbsp;&#8212;&nbsp;
-                <span className="study-field-name">{studyField}</span>
+              <span className="school-name">{educationInfo[schoolName]}</span>
+              &nbsp;&#8212;&nbsp;
+              <span className="study-field-name">{studyField}</span>
             </div>
             <div className="education-row-two">
-                <span className="start-date">{startDate}</span>
-                &nbsp;&#8211;&nbsp;
-                <span className="end-date">{endDate}</span>
-                &#44;&nbsp;
-                <span className="place">{place}</span>
+              <span className="start-date">{startDate}</span>
+              &nbsp;&#8211;&nbsp;
+              <span className="end-date">{endDate}</span>
+              &#44;&nbsp;
+              <span className="place">{place}</span>
             </div>
             <div className="education-row-three">
-            <p className="project-description">{description}</p>
+              <p className="project-description">{description}</p>
             </div>
-        </div>
-    )
+          </>
+        )}
+      </div>
+    );
 }
